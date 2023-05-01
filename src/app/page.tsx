0d156ab4d05a6716af397aa1209be7e5ac26cd92
ramjak2359 <rama.jakaria@2359media.com>
2023-05-01 23:24:14 +0800

    fixup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 "use client";
 import {useEffect, useMemo, useState} from "react";
 import styles from './page.module.scss'
-import {getDay} from "date-fns";
 import "react-datepicker/dist/react-datepicker.css";
-import Image from 'next/image'
-import {DatePicker, DatePickerProps, Select, TimePicker} from "antd";
+import {Card, DatePicker, DatePickerProps, Image, Select, TimePicker} from "antd";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faSun} from "@fortawesome/free-regular-svg-icons";
-import {faCarRear} from "@fortawesome/free-solid-svg-icons";
-import {AreaMetadata, Camera, TrafficDTO, WeatherDTO} from "@/types";
+import {
+  faCarRear,
+  faCloud,
+  faCloudBolt,
+  faCloudRain,
+  faCloudShowersHeavy, faMoon,
+  faQuestion
+} from "@fortawesome/free-solid-svg-icons";
+import {AreaMetadata, Camera, Forecast, TrafficDTO, WeatherDTO} from "@/types";
 import QueryString from 'query-string';
+import dayjs from "dayjs";
+import {IconDefinition} from "@ant-design/icons-svg/es/types";
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import {faSun} from "@fortawesome/free-regular-svg-icons";
 
 interface IPageProps {
 }
@@ -48,9 +56,6 @@ const Page = (props: IPageProps) => {
       })
     }
   };
-  const handleChange = (value: string | string[]) => {
-    console.log(`Selected: ${value}`);
-  };
 
   const [cameras, setCameras] = useState<Camera[]>();
   const loadTraffic = async (dateParam: Date) => {
@@ -66,6 +71,7 @@ const Page = (props: IPageProps) => {
   }
 
   const [locations, setLocations] = useState<AreaMetadata[]>();
+  const [weathers, setWeathers] = useState<Forecast[]>()
   const loadWeatherAndLocation = async (dateParam: Date) => {
     const TRAFFIC_API_URL = "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast";
     const dateString = dateParam.toISOString()
@@ -75,18 +81,72 @@ const Page = (props: IPageProps) => {
     const queryString = QueryString.stringify(query);
     const response = await fetch(TRAFFIC_API_URL+'?'+queryString);
     const jsonData: WeatherDTO = await response.json();
-    setLocations(jsonData.area_metadata);
+    jsonData.area_metadata?.length && setLocations(jsonData.area_metadata);
+    jsonData.items?.length && setWeathers(jsonData.items[0].forecasts)
   }
 
   useEffect(() => {
     loadTraffic(date);
     loadWeatherAndLocation(date);
   }, [date]);
-  console.log(locations);
 
   const locationOptions = useMemo(() => locations?.map(l => ({
     value: l.name, display: l.name,
   })) || [], [locations]);
+
+  const [selectedLocation, setSelectedLocation] = useState();
+  const selectedLocationDetail = useMemo(() => {
+    const metadata = locations?.find(l => l.name === selectedLocation);
+    const weather = weathers?.find(w => w.area === selectedLocation);
+
+    const nullCamera = { distance: Number.MAX_SAFE_INTEGER, camera: {
+        timestamp: '',
+        image: '',
+        location: {
+          longitude: 0,
+          latitude: 0,
+        },
+        camera_id: '',
+        image_metadata: {},
+      } }
+    const cameraData = metadata && cameras?.reduce((nearest, camera) => {
+      const latDist = Math.abs(camera.location.latitude - metadata.label_location.latitude)
+      const longDist = Math.abs(camera.location.longitude - metadata.label_location.longitude)
+      const distance = Math.sqrt((Math.pow(latDist, 2) + Math.pow(longDist, 2)))
+
+      if (distance < nearest.distance)
+        return { distance, camera }
+      else
+        return nearest
+    }, nullCamera)
+    return { metadata, weather, cameraData };
+  }, [selectedLocation, weathers, locations]);
+
+  const checkIsDisabled = (current: dayjs.Dayjs) => {
+    return current && current > dayjs().add(1, 'days')
+  };
+
+  const getWeatherIcon = (weather: string): IconProp => {
+    if (weather.match(/Thundery/)) {
+      return faCloudBolt
+    }
+    if (weather.match(/Shower/)) {
+      return faCloudShowersHeavy
+    }
+    if (weather.match(/Rain/)) {
+      return faCloudRain
+    }
+    if (weather.match(/Cloudy/)) {
+      return faCloud
+    }
+    if (weather.match(/Fair/) && weather.match(/Day/)) {
+      return faSun
+    }
+    if (weather.match(/Fair/) && weather.match(/Day/)) {
+      return faMoon
+    }
+    return faQuestion
+  }
 
   return (
     <main className={styles.main}>
@@ -99,20 +159,9 @@ const Page = (props: IPageProps) => {
         </p>
       </div>
 
-      <div className={styles.center}>
-        {/*<Image*/}
-        {/*  className={styles.logo}*/}
-        {/*  src="/next.svg"*/}
-        {/*  alt="Next.js Logo"*/}
-        {/*  width={180}*/}
-        {/*  height={37}*/}
-        {/*  priority*/}
-        {/*/>*/}
-      </div>
-
       <div className={styles.grid}>
         <div>
-          <DatePicker onChange={onChange} className={styles.input} size="large" />
+          <DatePicker onChange={onChange} className={styles.input} size="large" disabledDate={checkIsDisabled} />
         </div>
         <div>
           <TimePicker onChange={onTimeChange} className={styles.input} size="large" />
@@ -120,11 +169,28 @@ const Page = (props: IPageProps) => {
         <div>
           <Select
             size="large"
-            onChange={handleChange}
+            onChange={setSelectedLocation}
             options={locationOptions}
             disabled={!date}
             className={styles.input}
           />
+        </div>
+        <div>
+          <Card style={{ width: '100%' }}>
+            {
+              selectedLocationDetail.weather && <>
+                <div><FontAwesomeIcon icon={getWeatherIcon(selectedLocationDetail.weather.forecast)} size="2x"  /></div>
+                <p>{selectedLocationDetail.weather.forecast}</p>
+              </>
+            }
+          </Card>
+        </div>
+        <div>
+          {selectedLocationDetail.cameraData && <Image
+            src={selectedLocationDetail.cameraData?.camera.image}
+            className={styles.trafficImg}
+          />
+          }
         </div>
       </div>
     </main>
